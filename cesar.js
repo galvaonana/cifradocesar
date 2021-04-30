6e48573ffd58e0574bb70c35bb97268a42fb43df
Natalia Galvao <nreginagal@gmail.com>
2021-04-29 21:34:19 -0300

    correcao do java script

--- cesar.js
+++ cesar.js
@@ -6,27 +6,27 @@ function inicio() {
     }, true);
     document.getElementById("cifrar").addEventListener("click", function(){
         let texto = document.getElementById("mensagem").value;
-        let descriptogafia = document.getElementById("descriptografia").value;
-        document.getElementById("mensagem2").value = cifrar(texto, descriptogafia);
+        let deslocamento = document.getElementById("deslocamento").value;
+        document.getElementById("mensagem2").value = cifrar(texto, deslocamento);
     }, true);
     document.getElementById("decifrar").addEventListener("click", function(){
-        let texto = document.getElementById("mensagem".value);
-        let descriptogafia = document.getElementById("descriptografia").value;
-        document.getElementById("mensagem2").value = decifrar(texto, descriptogafia);
+        let texto = document.getElementById("mensagem").value;
+        let deslocamento = document.getElementById("deslocamento").value;
+        document.getElementById("mensagem2").value = decifrar(texto, deslocamento);
         },true);
     }
 
-    function cifrar(texto, descriptogafia){
+    function cifrar(texto, deslocamento){
         let resultado = "";
-        let letras = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+        let alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
 
-        descriptogafia = (descriptogafia % 26 + 26) % 26;
+        deslocamento = (deslocamento % 26 + 26) % 26;
 
         if (texto){
-            for (let i = 0; i<texto.length;i++){
-                if (letras.indexOf(texto[i] !=-1)){
-                    let posicion = ((letras.indexOf(texto[i])+descriptografia)%26);
-                    resultado += letras[posicion];
+            for (let i = 0; i<texto.length; i++){
+                if (alfabeto.indexOf(texto[i])!=-1){
+                    let posicao = ((alfabeto.indexOf(texto[i])+deslocamento)%26);
+                    resultado += alfabeto[posicao];
                 }
                 else
                     resultado += texto[i];   
@@ -35,20 +35,19 @@ function inicio() {
        return resultado; 
     }
 
-    function cifrar2 (texto, descriptogafia){
-        if (!texto){
+    function cifrar2 (texto, deslocamento){
+        if (!texto)
             return "";
-            const letras = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
-            descriptogafia = (descriptogafia % 26 + 26) % 26;
-            return texto.replace (/[A-Z]/ig, c=> letras[(letras.indexOf(c)+descriptogafia)%26]);
-        }
-        function decifrar (texto, descriptogafia){
-            if (!texto){
-                return "";
-                const letras = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
-                descriptogafia = (descriptogafia % 26 - 26) % 26;
-                return texto.replace (/[A-Z]/ig, c=> letras[(letras.indexOf(c)-descriptogafia)%26]);
-            }
+        const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+        deslocamento = (deslocamento % 26 + 26) % 26;
+        return texto.replace(/[A-Z]/ig, c=> alfabeto[(alfabeto.indexOf(c)+deslocamento)%26]);
+    }
+    
+    function decifrar (texto, deslocamento){
+        if (!texto)
+            return "";
+        const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+        deslocamento = (deslocamento % 26 - 26) % 26;
+        return texto.replace (/[A-Z]/ig, c=> alfabeto[(alfabeto.indexOf(c)-deslocamento)%26]);
     }
     //a mensagem não está mais aparecendo undefined mas também nada aparece feijoada
-    
